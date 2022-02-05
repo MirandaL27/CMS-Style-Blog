1e963b6fb5b16e27ee9cb66e2075cd4fbf457009
Cadenza <mirandalucsb@gmail.com>
2022-02-04 19:11:49 -0700

    finished user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,13 +2,54 @@ const router = require('express').Router();
 const { User, Post, Vote, Comment} = require("../../models");
 
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    try{
+        let data = await User.findAll({
+            attributes: { exclude: ['password'] }
+          });
 
+          res.json(data)   
+    }
+    catch(error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
 });
 
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+    try{
+        let data = await User.findOne({
+            attributes: { exclude: ['password'] },
+            where: {
+              id: req.params.id
+            },
+            include: [
+              {
+                model: Post,
+                attributes: ['id', 'title', 'post_content', 'created_at']
+              },
+              {
+                model: Comment,
+                attributes: ['id', 'comment_contents', 'created_at'],
+                include: {
+                  model: Post,
+                  attributes: ['title']
+                }
+              },
+            ]
+          });
 
+        if (!data) {
+            res.status(404).json({ message: 'No user found with this id' });
+        return;
+        }
+        res.json(data);
+    }
+    catch(error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
 });
 
 
@@ -34,21 +75,86 @@ router.post('/',async (req, res) => {
     }
 });
 
-router.post('/login', (req, res) => {
-  
+router.post('/login', async (req, res) => {
+    try{
+        let data = await User.findOne({
+            where: {
+              email: req.body.email
+            }
+          });
+          if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+          }
+          const validPass = data.comparePassword(req.body.password);
+          if (!validPass) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+          }  
+          req.session.save(() => {
+            // declare session variables
+            req.session.user_id = data.id;
+            req.session.username = data.user_name;
+            req.session.loggedIn = true;
+            });
+      
+            res.json({ user: data, message: 'You are now logged in!' });
+    }
+    catch(error){
+        console.log(error);
+    }
 });
 
 router.post('/logout', (req, res) => {
-
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+          res.status(204).end();
+        });
+      }
+      else {
+        res.status(404).end();
+      }
 });
 
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+    try{
+        let data = await User.update(req.body, {
+            individualHooks: true,
+            where: {
+              id: req.params.id
+            }
+          });
+          if (!data[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+          }
+          res.json(data);  
 
-  });
+    }
+    catch(error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async(req, res) => {
+    try{
+        let data = await User.destroy({
+            where: {
+              id: req.params.id
+            }
+          });
+          if (!data) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+          }
+          res.json(data);      
+    }
+    catch(error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
   });
 
 module.exports = router;
