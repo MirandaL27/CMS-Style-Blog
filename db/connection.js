30f5cd40f0871db3324c67bafcf2f2ad0a89c89f
Cadenza <mirandalucsb@gmail.com>
2022-02-22 16:16:43 -0700

    added connection stuff for heroku

--- db/connection.js
+++ db/connection.js
@@ -2,11 +2,16 @@ const Sequelize = require('sequelize'); //need sequelize to make the connection
 
 require('dotenv').config(); //need access to environment variables to keep the username/password safe
 
-// create connection to the database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, { // pass in database name, username and password
-  host: 'localhost', // connection server
-  dialect: 'mysql',//sql language dialect
-  port: 3306 // port connection is on
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 
 module.exports = sequelize; //export connection object so it can be used in other files
